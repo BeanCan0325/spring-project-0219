fc4b85755a63d9d8b679ae861bf6cd92d5ae59da
Bora <sbr7518@gmail.com>
2026-02-20 17:04:07 +0900

    spring_project.entity:Member(내용추가)

--- src/main/java/kr/co/spring_project/entity/Member.java
+++ src/main/java/kr/co/spring_project/entity/Member.java
@@ -1,5 +1,46 @@
 package kr.co.spring_project.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long memberId;
+	private String userId;
+	private String password;
+	private String userName;
+	private LocalDateTime birthDate;
+	private String phone;
+	
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = LocalDateTime.now(); 
+	}
+	
 }
