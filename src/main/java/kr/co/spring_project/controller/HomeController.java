174faa2594632c0ea9c74e770660b620b8270f5d
Bora <sbr7518@gmail.com>
2026-02-21 14:20:28 +0900

    java c c p

--- src/main/java/kr/co/spring_project/controller/HomeController.java
+++ src/main/java/kr/co/spring_project/controller/HomeController.java
@@ -2,11 +2,16 @@ package kr.co.spring_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+<<<<<<< HEAD
+=======
+
+>>>>>>> d86e514 (Resolve merge conflict)
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+<<<<<<< HEAD
         return "home"; // home.html 반환
     }
 
@@ -16,3 +21,9 @@ public class HomeController {
     }
 
 }
+=======
+
+        return "home";
+    }
+}
+>>>>>>> d86e514 (Resolve merge conflict)
