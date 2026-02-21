4717175e4a0e02e8d96082a558c4ba9c9e07013f
Hello-pythworld <128715051+Hello-pythworld@users.noreply.github.com>
2026-02-21 16:37:03 +0900

    "책반납완료까지"

--- src/main/java/kr/co/spring_project/service/ReturnService.java
+++ src/main/java/kr/co/spring_project/service/ReturnService.java
@@ -1,5 +1,9 @@
 package kr.co.spring_project.service;
 
-public class ReturnService {
+import kr.co.spring_project.dto.returnbook.ReturnRequestDTO;
+
+public interface ReturnService {
+
+	void returnBook(ReturnRequestDTO returnRequestDTO);
 
 }
--- src/main/java/kr/co/spring_project/serviceImpl/ReturnServiceImpl.java
+++ src/main/java/kr/co/spring_project/serviceImpl/ReturnServiceImpl.java
@@ -1,5 +1,27 @@
 package kr.co.spring_project.serviceImpl;
 
-public class ReturnServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.spring_project.dto.returnbook.ReturnRequestDTO;
+import kr.co.spring_project.entity.Book;
+import kr.co.spring_project.repository.ReturnRepository;
+import kr.co.spring_project.service.ReturnService;
+
+@Service
+public class ReturnServiceImpl implements ReturnService{
+	
+	@Autowired
+	private ReturnRepository returnRepository;
+
+	@Override
+	public void returnBook(ReturnRequestDTO returnRequestDTO) {
+		int bookId = returnRequestDTO.getBookId();
+		
+		Book book = returnRepository.findById(bookId).orElseThrow();
+		book.setStatus("반납완료"); // 상태 변경
+//		반납 처리 로직
+		returnRepository.save(book);
+	}
 
 }
