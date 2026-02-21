297e11ac23ed962c64afdc3e72bcfec640e5ab2f
beanCan <bean.can0325@gmail.com>
2026-02-21 18:26:31 +0900

    Feat:Borrowing controller 생성, borrowing ,book entity 생성

--- src/main/java/kr/co/spring_project/entity/Book.java
+++ src/main/java/kr/co/spring_project/entity/Book.java
@@ -1,5 +1,25 @@
 package kr.co.spring_project.entity;
 
-public class Book {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
+@Table(name = "books")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bookId;
+	
+	private String title;
+	
+	private String author;
+	
+	private String publisher;
 }
--- src/main/java/kr/co/spring_project/entity/Borrowing.java
+++ src/main/java/kr/co/spring_project/entity/Borrowing.java
@@ -1,5 +1,31 @@
 package kr.co.spring_project.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Borrowing {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long borrowKey;
+	
+	
+	@ManyToOne
+	@JoinColumn(name ="member_id")
+	private Member member;
+	
+	@ManyToOne
+	@JoinColumn(name ="book_id")
+	private Book book;
+	
+	private boolean isBorrowed;
+	
 }
