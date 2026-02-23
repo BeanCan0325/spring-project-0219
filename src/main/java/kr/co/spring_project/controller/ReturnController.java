d339374293f681263705176ffe8eee39c243ebe5
beanCan <bean.can0325@gmail.com>
2026-02-23 10:53:55 +0900

    Feat: 비로그인시 대여, 반납버튼 누르면 로그인창으로 이

--- src/main/java/kr/co/spring_project/controller/ReturnController.java
+++ src/main/java/kr/co/spring_project/controller/ReturnController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.servlet.http.HttpSession;
 import kr.co.spring_project.dto.member.ResLoginDTO;
-import kr.co.spring_project.entity.Borrowing;
 import kr.co.spring_project.dto.returnbook.ReturnRequestDTO;
+import kr.co.spring_project.entity.Borrowing;
 import kr.co.spring_project.service.BorrowingService;
 import kr.co.spring_project.service.ReturnService;
 import lombok.RequiredArgsConstructor;
@@ -23,9 +23,14 @@ public class ReturnController {
     @GetMapping("/return")
     public String returnPage(HttpSession session, Model model) {
         ResLoginDTO loginUser = (ResLoginDTO) session.getAttribute("LOGIN_USER");
+
+        if(loginUser == null) {
+            return "redirect:/member/login";
+        }
+
         List<Borrowing> borrowings = borrowingService.getMyBorrowings(loginUser.getMemberId());
         model.addAttribute("borrowings", borrowings);
-        model.addAttribute("userName", loginUser.getUserName()); 
+        model.addAttribute("userName", loginUser.getUserName());
         return "return";
     }
 
